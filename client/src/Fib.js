8c0698d28c0094a41bf1219f2e28a7d7bf4aaa8e
Pieter Holthuijsen <pieter.holthuijsen@nn-group.com>
2020-01-16 13:54:05 +0100

    Replace Fib.js from trainer. Did not see the problem myself :<)

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -24,53 +24,51 @@ class Fib extends Component {
       seenIndexes: seenIndexes.data
     });
   }
-  
+
   handleSubmit = async event => {
     event.preventDefault();
-    
+
     await axios.post('/api/values', {
       index: this.state.index
     });
-    
     this.setState({ index: '' });
   };
-  
+
   renderSeenIndexes() {
-    return this.state.seenIndexes.map(({ number }) => number).join(', '); 
+    return this.state.seenIndexes.map(({ number }) => number).join(', ');
   }
-  
+
   renderValues() {
     const entries = [];
-  
+
     for (let key in this.state.values) {
       entries.push(
         <div key={key}>
           For index {key} I calculated {this.state.values[key]}
         </div>
-      )
+      );
     }
-  
+
     return entries;
   }
 
-  render () {
+  render() {
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
-          <label>Enter your value:</label>
-          <input 
+          <label>Enter your index:</label>
+          <input
             value={this.state.index}
             onChange={event => this.setState({ index: event.target.value })}
           />
           <button>Submit</button>
         </form>
-        
+
         <h3>Indexes I have seen:</h3>
-        {this.renderSeenIndexes()}        
-        
+        {this.renderSeenIndexes()}
+
         <h3>Calculated Values:</h3>
-        {this.renderValues()}        
-        
+        {this.renderValues()}
       </div>
     );
   }
